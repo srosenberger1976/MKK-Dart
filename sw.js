db78cb1b47c2351c9e07ba7384d86ed3094e5811
srosenberger1976 <s.rosenberger1976@gmail.com>
2026-09-21 13:32:10 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,8 +1,14 @@
 const CACHE_NAME = 'app-cache-v1';
 const ASSETS = ['./', './index.html', './manifest.json'];
+
 self.addEventListener('install', (event) => {
-event.waitUntil(caches.open(CACHE_NAME).then((c) => c.addAll(ASSETS)));
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
+  );
 });
+
 self.addEventListener('fetch', (event) => {
-event.respondWith(caches.match(event.request).then((res) => res || fetch(event.request)));
+  event.respondWith(
+    caches.match(event.request).then((res) => res || fetch(event.request))
+  );
 });
