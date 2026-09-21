d232deb34c151c8580f671616c39a58e6420192c
srosenberger1976 <s.rosenberger1976@gmail.com>
2026-09-21 16:26:58 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,14 +1,6 @@
-const CACHE_NAME = 'app-cache-v1';
-const ASSETS = ['./', './index.html', './manifest.json'];
-
-self.addEventListener('install', (event) => {
-  event.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
-  );
-});
-
-self.addEventListener('fetch', (event) => {
-  event.respondWith(
-    caches.match(event.request).then((res) => res || fetch(event.request))
-  );
+const CACHE_NAME = 'dart-bogen-v2';
+self.addEventListener('install', (e) => self.skipWaiting());
+self.addEventListener('activate', (e) => e.waitUntil(self.clients.claim()));
+self.addEventListener('fetch', (e) => {
+    e.respondWith(fetch(e.request).catch(() => caches.match(e.request)));
 });
